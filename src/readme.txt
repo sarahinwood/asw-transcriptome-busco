1d71e33285dc0ad2a02f8dc6b5fecba64c8ea777
Sarah Inwood <sninwood@gmail.com>
2017-09-04 13:44:44 +1200

    other scripts

---
+++ src/readme.txt
@@ -0,0 +1,10 @@
+BUSCO analysis of de novo transcriptome assembly using Trinity
+(for assembly see /Volumes/BiocArchive/archive/deardenlab/asw-transcriptome)
+
+1 - 3. Run BUSCO analyses as appropriate
+	run_busco_transcriptome_tribolium
+	run_busco_transcriptome_by_expression_tribolium
+	run_busco_transcriptome_by_length_tribolium
+
+4. compare results from BUSCO analyses
+	busco_analysis.R
--- src/run_busco_transcriptome_by_length_tribolium.sh
+++ src/run_busco_transcriptome_by_length_tribolium.sh
@@ -3,8 +3,8 @@ export PATH="${HOME}/local/bin:${HOME}/local/bin/augustus/bin:${HOME}/local/bin/
 export AUGUSTUS_CONFIG_PATH="${HOME}/local/bin/augustus/config"
 
 python bin/busco/BUSCO.py \
---in data/Trinity.fasta \
---out asw_transcriptome2 \
+--in data/isoforms_by_length.fasta \
+--out asw_transcriptome2_filtered_by_length \
 --lineage data/endopterygota_odb9 \
 --mode transcriptome \
 --cpu 10 --species tribolium2012
